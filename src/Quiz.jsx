4042783a422b2a953c3f7590d98eadd91f06d98d
Rifadh <rifadhrijan@gmail.com>
2024-12-03 16:56:09 +0530

    Quiz and Answers with animation & sound

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
 import Trivia from './components/Trivia';
+import Timer from './components/Timer';
 
 function Quiz() {
   const [questionNumber, setQuestionNumber] = useState(1);
@@ -76,23 +77,30 @@ function Quiz() {
     },
   ];
 
-  const moneyPyramid = [
-    { id: 1, amount: '$ 100' },
-    { id: 2, amount: '$ 200' },
-    { id: 3, amount: '$ 300' },
-    { id: 4, amount: '$ 500' },
-    { id: 5, amount: '$ 1000' },
-    { id: 6, amount: '$ 2000' },
-    { id: 7, amount: '$ 4000' },
-    { id: 8, amount: '$ 8000' },
-    { id: 9, amount: '$ 16000' },
-    { id: 10, amount: '$ 32000' },
-    { id: 11, amount: '$ 64000' },
-    { id: 12, amount: '$ 125000' },
-    { id: 13, amount: '$ 250000' },
-    { id: 14, amount: '$ 500000' },
-    { id: 15, amount: '$ 1000000' },
-  ].reverse();
+  const moneyPyramid = useMemo(() => 
+    [
+      { id: 1, amount: '$ 100' },
+      { id: 2, amount: '$ 200' },
+      { id: 3, amount: '$ 300' },
+      { id: 4, amount: '$ 500' },
+      { id: 5, amount: '$ 1000' },
+      { id: 6, amount: '$ 2000' },
+      { id: 7, amount: '$ 4000' },
+      { id: 8, amount: '$ 8000' },
+      { id: 9, amount: '$ 16000' },
+      { id: 10, amount: '$ 32000' },
+      { id: 11, amount: '$ 64000' },
+      { id: 12, amount: '$ 125000' },
+      { id: 13, amount: '$ 250000' },
+      { id: 14, amount: '$ 500000' },
+      { id: 15, amount: '$ 1000000' },
+    ].reverse(),
+   []);
+
+  useEffect(() => {
+    questionNumber > 1 &&
+      setEarned(moneyPyramid.find((m) => m.id === questionNumber - 1).amount);
+  }, [moneyPyramid, questionNumber]);
 
   return (
     <>
@@ -100,11 +108,13 @@ function Quiz() {
       <div className="main">
         <div className="quiz">
           {stop ? (
-            <h1>You Earned: {earned}</h1>
+            <h1 className="endText">You Earned: {earned}</h1>
           ) : (
             <>
               <div className="top">
-                <div className="timer">30</div>
+                <div className="timer">
+                    <Timer setStop={setStop} questionNumber={questionNumber} />
+                </div>
               </div>
 
               <div className="bottom">
--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,9 +1,20 @@
 import { useEffect, useState } from 'react';
+import useSound from 'use-sound';
+import play from '../assets/sounds/play.mp3';
+import correct from '../assets/sounds/correct.mp3';
+import wrong from '../assets/sounds/wrong.mp3';
 
 function Trivia({ data, setStop, questionNumber, setQuestionNumber }) {
   const [question, setQuestion] = useState(null);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
-  const [className, setClassName] = useState("answer")
+  const [className, setClassName] = useState('answer');
+  const [letsPlay] = useSound(play);
+  const [correctAnswer] = useSound(correct);
+  const [wrongAnswer] = useSound(wrong);
+
+  useEffect(() => {
+    letsPlay();
+  }, [letsPlay]);
 
   useEffect(() => {
     setQuestion(data[questionNumber - 1]);
@@ -11,23 +22,30 @@ function Trivia({ data, setStop, questionNumber, setQuestionNumber }) {
 
   const delay = (duration, callback) => {
     setTimeout(() => {
-        callback();
+      callback();
     }, duration);
   };
 
   const handleClick = (a) => {
     setSelectedAnswer(a);
-    setClassName("answer active");
-    delay(3000, () => setClassName(a.correct ? "answer correct" : "answer wrong"));
-    delay(6000, () => {
-        if(a.correct){
-            setQuestionNumber((prev) => prev + 1);
-            setSelectedAnswer(null);
-        }
-        else{
-            setStop(true)
-        }
-    } );  
+    setClassName('answer active');
+    delay(3000, () =>
+      setClassName(a.correct ? 'answer correct' : 'answer wrong')
+    );
+    delay(5000, () => {
+      if (a.correct) {
+        correctAnswer();
+        delay(1000, () => {
+          setQuestionNumber((prev) => prev + 1);
+          setSelectedAnswer(null);
+        });
+      } else {
+        wrongAnswer();
+        delay(1000, () => {
+          setStop(true);
+        });
+      }
+    });
   };
 
   return (
@@ -35,7 +53,11 @@ function Trivia({ data, setStop, questionNumber, setQuestionNumber }) {
       <div className="question">{question?.question}</div>
       <div className="answers">
         {question?.answers.map((a) => (
-          <div key={a.id} className={selectedAnswer === a ? className : "answer"} onClick={() => handleClick(a)}>
+          <div
+            key={a.id}
+            className={selectedAnswer === a ? className : 'answer'}
+            onClick={() => handleClick(a)}
+          >
             {a.text}
           </div>
         ))}
